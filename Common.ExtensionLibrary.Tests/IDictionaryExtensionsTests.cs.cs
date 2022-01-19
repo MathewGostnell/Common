8e436a5fb12b4b93369afb211584a34fd8e35ca3
Mathew Gostnell <mathew.gostnell@gmail.com>
2022-01-18 17:36:05 -0700

    Added IDictionary extension method and test classes.

---
+++ Common.ExtensionLibrary.Tests/IDictionaryExtensionsTests.cs.cs
@@ -0,0 +1,68 @@
+﻿namespace Common.ExtensionLibrary.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Shouldly;
+    using System;
+    using System.Collections.Generic;
+
+    [TestClass]
+    public class IDictionaryExtensionsTests
+    {
+        [TestMethod]
+        public void AddOrUpdate_ExistingEntry_ReturnsFalse()
+        {
+            string key = string.Empty;
+            int value = 1;
+            var dictionary = new Dictionary<string, int>
+            { 
+                { key, value }
+            };
+
+            value = 5;
+            var result = dictionary.AddOrUpdate(
+                key,
+                value);
+
+            result.ShouldBeFalse();
+            dictionary[key].ShouldBe(value);
+        }
+
+        [TestMethod]
+        public void AddOrUpdate_NewEntry_ReturnsTrue()
+        {
+            var dictionary = new Dictionary<string, int>();
+            string key = string.Empty;
+            int value = 1;
+
+            var result = dictionary.AddOrUpdate(
+                key,
+                value);
+
+            result.ShouldBeTrue();
+            dictionary[key].ShouldBe(value);
+        }
+
+        [TestMethod]
+        public void AddOrUpdate_NewEntryWithoutValue_StoresDefaultValue()
+        {
+            var dictionary = new Dictionary<string, int>();
+            string key = string.Empty;
+            int value = default(int);
+
+            var result = dictionary.AddOrUpdate(key);
+
+            result.ShouldBeTrue();
+            dictionary[key].ShouldBe(value);
+        }
+
+        [TestMethod]
+        public void AddOrUpdate_NullDictionary_ThrowsNullReferenceException()
+        {
+            IDictionary<string, int> dictionary = null;
+
+            Should.Throw<NullReferenceException>(
+                () => 
+                dictionary.AddOrUpdate("throws exception", -1));
+        }
+    }
+}
---
+++ Common.ExtensionLibrary/IDictionaryExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Common.ExtensionLibrary
+{
+    using System.Collections.Generic;
+
+    public static class IDictionaryExtensions
+    {
+        public static bool AddOrUpdate<TKey, TValue>(
+            this IDictionary<TKey, TValue?> dictionary,
+            TKey key,
+            TValue? value = default)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                dictionary[key] = value;
+                return false;
+            }
+
+            dictionary.Add(
+                key,
+                value);
+            return true;
+        }
+    }
+}
