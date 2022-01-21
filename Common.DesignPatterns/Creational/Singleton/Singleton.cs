ee8ff1b1e49d1b7d56f1aaf5e9ee3364e31cb579
Mathew Gostnell <mathew.gostnell@gmail.com>
2022-01-20 17:50:48 -0700

    * Added Singleton creational design pattern with unit test coverage

---
+++ Common.DesignPatterns/Creational/Singleton/Singleton.cs
@@ -0,0 +1,24 @@
+﻿namespace Common.DesignPatterns.Creational.Singleton
+{
+    public abstract class Singleton<TInstance>
+        where TInstance : notnull, new()
+    {
+        protected static TInstance? Instance
+        
+        {
+            get;
+            private set;
+        }
+
+        // TODO Relax type constraints to force consumer to have protected constructor?
+        public static TInstance GetInstance()
+        {
+            if (Instance is null)
+            {
+                Instance = new TInstance();
+            }
+
+            return Instance;
+        }
+    }
+}
