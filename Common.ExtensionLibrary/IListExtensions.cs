be0582ab574618d750fbae8376941a39f1e08264
Mathew Gostnell <mathew.gostnell@gmail.com>
2022-01-21 22:32:56 -0700

    * Added extension method to nicely check for null or not Any() for lists * Tested logic with added unit tests

---
+++ Common.ExtensionLibrary/IListExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Common.ExtensionLibrary
+{
+    public static class IListExtensions
+    {
+        public static bool IsEmpty<TElement>(
+            this IList<TElement> elementList)
+            => !elementList.Any();
+
+        public static bool IsNullOrEmpty<TElement>(
+            this IList<TElement>? elementList)
+            => elementList is null
+                || elementList.IsEmpty();
+    }
+}
